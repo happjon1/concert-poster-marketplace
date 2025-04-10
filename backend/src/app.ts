7fd268cdd3d920a08abc8969ea534cb87f6fe431
happjon1 <314502+happjon1@users.noreply.github.com>
2025-04-09 21:04:07 -0400

    make cors more permissive in backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,14 @@ dotenv.config();
 const app: Application = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://tubebazaar.com", "http://localhost:4200"], // Allow your domains
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
