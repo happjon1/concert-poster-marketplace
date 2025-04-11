852f2d9b97b4a64a6520b745d0eab3920c72c3d5
happjon1 <314502+happjon1@users.noreply.github.com>
2025-04-10 21:10:55 -0400

    remove api from urls

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // frontend/src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://tubebazaar.com/api',
+  apiUrl: 'https://tubebazaar.com',
   appName: 'Tube Bazaar',
   defaultPageSize: 12,
   imageStorageUrl: 'https://tubebazaar.com/api/uploads',
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'http://localhost:3000',
 
   // Optional additional configuration
   appName: 'Concert Poster Marketplace',
